9c7805a6139b578a269a2899270c3721da3b95ee
Endy3032 <endy@endy3032.ml>
2023-08-25 03:07:15 +0700

    update regex

--- Uptime.jsx
+++ Uptime.jsx
@@ -5,7 +5,7 @@ const options = {
 	top: "14.75%",
 	left: "8%",
 	size: "0.8vw",
-};
+}
 
 export const refreshFrequency = 20e3
 export const command = "uptime"
@@ -21,9 +21,14 @@ export const className = `
 `
 
 export const render = ({ output }) => {
-	const matchedDate = output.match(/\d+ days?/) ?? [""]
-	const matchedTime = output.match(/ (\d+):(\d+)/) ?? output.match(/()(\d+) mins/) ?? [""]
-	const d = matchedDate[0].match(/\d+/)
-	const [h, m] = matchedTime.filter(e => e.length <= 2)
-	return <span className={css(`font-size: ${options.size}`)}>Up {d ? `${d}d ` : ""}{h ? `${h}h ` : ""}{m}m</span>
+	const d = (output.match(/\d+(?= days?)/) ?? [""])[0]
+	const [, h, m] = output.match(/()(\d+)(?= mins?)/) ?? output.match(/(?<= )(\d{0,2}):(\d{0,2})/) ?? [""]
+
+	return (
+		<span className={css(`font-size: ${options.size}`)}>
+			Up {d ? `${d}d ` : ""}
+			{h ? `${h}h ` : ""}
+			{m}m
+		</span>
+	)
 }
